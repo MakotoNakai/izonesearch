e3e2fd5c416697087ad97188d96470571bfc50b7
Makoto Nakai <macofandohr1812@gmail.com>
2020-08-14 22:54:39 +0900

    main.py updated

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def post():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, threaded=True)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, threaded=True)
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
     
