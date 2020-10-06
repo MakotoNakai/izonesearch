504a73d56f34d7f74f6f3dbb8a8af05908bfa12b
Makoto Nakai <macofandohr1812@gmail.com>
2020-10-06 17:07:01 +0900

    updated requirement.txt

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def post():
 
     # 選んだメンバーの名前を取得
     name = request.form.get("name")
-    num_pics = int(request.form.get("num_pics"))
+    num_pics = request.form.get("num_pics")
 
 
     select_name = []
@@ -32,21 +32,22 @@ def post():
                 "Chewon_Kim", "Minju_Kim", "Nako_Yabuki", "Hitomi_Honda", "Yuri_Choi", "Yujin_Ahn", "Wonyoung_Chang"]
     name_list = ["Kwon Eunbi", "Sakura Miyawaki", "Hyewon Kang", "Yena Choi", "Cheyeon Lee", \
                 "Chewon Kim", "Minju Kim", "Nako Yabuki", "Hitomi Honda", "Yuri Choi", "Yujin Ahn", "Wonyoung hang"]
-    option_list_name = [[value, name, None] for value, name in zip(value_list, name_list)]
-    for idx in range(len(value_list)):
-        if name_list[idx] == name:
-            option_list_name[idx][2] = 'selected'
+    
+    option_list_name = [[value, name] for value, name in zip(value_list, name_list)]
+    # for idx in range(len(value_list)):
+    #     if name_list[idx] == name:
+    #         option_list_name[idx][2] = 'selected'
 
     
     num_pics_list = [1, 5, 10, 20]
-    option_list_num_pics = [[num, num, None] for num in num_pics_list]
-    for idx in range(len(num_pics_list)):
-        if num_pics_list[idx] == num_pics:
-            option_list_num_pics[idx][2] = 'selected'
+    option_list_num_pics = [[str(num), str(num)] for num in num_pics_list]
+    # for idx in range(len(num_pics_list)):
+    #     if num_pics_list[idx] == num_pics:
+    #         option_list_num_pics[idx][2] = 'selected'
 
     file_list = []
 
-    id_list = [random.randint(0, 40) for i in range(num_pics)]
+    id_list = [random.randint(0, 40) for i in range(int(num_pics))]
     for id_ in id_list:
     
         query = "SELECT * FROM izonetable WHERE member = %s AND id = %s"
@@ -73,7 +74,7 @@ def post():
             cursor.close()
             connect.close()
 
-    return render_template("result.html", option_list_name=option_list_name, option_list_num_pics=option_list_num_pics, file_list=file_list)
+    return render_template("result.html", name=name, num_pics=num_pics, option_list_name=option_list_name, option_list_num_pics=option_list_num_pics, file_list=file_list)
 
 
 
