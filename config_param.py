93a8966087f878606fda192cdd09cce7103f4c4f
MakotoNakai <macofandohr1812@gmail.com>
2021-02-06 22:47:25 +0900

    removed comments

--- config_param.py
+++ config_param.py
@@ -1,6 +1,4 @@
 
-# 表示する画像のメンバー名の選択肢
 name_list = ["Eunbi", "Sakura", "Hyewon", "Yena", "Cheyeon", "Chewon", "Minju", "Nako", "Hitomi", "Yuri", "Yujin", "Wonyoung"]
 
-# 画像の枚数の選択肢
 num_pics_list = [1, 5, 10, 20]
